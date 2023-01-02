f4e6f77768e7d1ecda8a9c0ca9c1aa287d974bcc
Jun Suzuki <junsuzuki@gmx.fr>
2023-01-02 22:46:30 +0100

    Add interview question in comment

--- maxSubarray.sol
+++ maxSubarray.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+// Interview question of the week by Cassidy Williams (issue #281) :
+//
+// Given an array of integers arr and an integer n, return a subarray of
+// arr of length n where the sum is the largest. Make sure you maintain
+// the order of the original array, and if n is greater than arr.length,
+// you can choose what you want to return.
+
 contract main {
     function sum(int256[] memory arr) private pure returns (int256) {
         int256 res = 0;
