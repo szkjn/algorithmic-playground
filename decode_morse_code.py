1fcbba5a56742ff122de085c565eee859657ed25
Jun Suzuki <84317349+szkjn@users.noreply.github.com>
2023-01-06 12:09:31 +0100

    Create decode_morse_code.py

---
+++ decode_morse_code.py
@@ -0,0 +1,34 @@
+"""
+Code Wars Kata :
+
+Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+"""
+
+MORSE_CODE = {
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F',
+    '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L',
+    '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R',
+    '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
+    '-.--': 'Y', '--..': 'Z',
+    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4',
+    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9',
+    '.-.-.-': '.', '--..--': ',', '..--..': '?', '.----.': "'", '-.-.--': '!',
+    '-..-.': '/', '-.--.': '(', '-.--.-': ')', '.-...': '&', '---...': ':',
+    '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', '..--.-': '_',
+    '.-..-.': '"', '...-..-': '$', '.--.-.': '@', '...---...': 'SOS'
+}
+
+
+def decode_morse(morse_code): 
+    words = morse_code.strip().split('   ')
+    result = []
+    
+    for word in words: 
+        result.append(''.join([MORSE_CODE[letter] for letter in word.split(' ')]))
+
+    return ' '.join(result)
+  
+  
+decode_morse('.... . -.--   .--- ..- -.. .')  # output: 'HEY JUDE'
+decode_morse('...---...')  # output : 'SOS'
+decode_morse('...   ---   ...')  # output: 'S O S'
