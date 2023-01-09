dc76d26dcfc6117756f5d7a3a360d73e631b20dc
Jun Suzuki <84317349+szkjn@users.noreply.github.com>
2023-01-09 14:02:12 +0100

    Commit Python version

---
+++ sum_every_other.py
@@ -0,0 +1,21 @@
+"""
+Interview question of the week by Cassidy Williams (issue #282) :
+
+Given a number, sum every second digit in that number.
+Example:
+> sumEveryOther(548915381)
+> 26 4+9+5+8
+> sumEveryOther(10)
+> 0
+> sumEveryOther(1010.11)
+> 1 // 0+0+1
+"""
+
+def sum_every_other(n: float) -> int:
+    n = str(n).replace('.', '')
+    result = [int(n[char]) for char in range(1, len(n), 2)]
+    print(sum(result))
+
+sum_every_other(548915381)
+sum_every_other(10)
+sum_every_other(1010.11)
