e40003ba09e83d86c829fa2a7261fb9ad5c7d07f
Jun Suzuki <84317349+szkjn@users.noreply.github.com>
2023-02-07 10:42:19 +0100

    Create get_a1_ref_num.py

---
+++ get_a1_ref_num.py
@@ -0,0 +1,31 @@
+"""
+Interview question of the week by Cassidy Williams (issue #286) :
+
+Spreadsheet programs often use the A1 Reference Style to refer to
+columns. Given a column name in this style, return its column number.
+
+Examples of column names to their numbers:
+A -> 1
+B -> 2
+C -> 3
+Z -> 26
+AA -> 27
+AB -> 28 
+AAA -> 703
+"""
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def column_number(col_name):
+  col_num = 0
+  for c in col_name:
+    col_num = col_num * len(alphabet) + alphabet.index(c) + 1 
+  
+  print(col_num)
+
+column_number("A")
+column_number("B")
+column_number("Z")
+column_number("AA")
+column_number("AB")
+column_number("AAA")
