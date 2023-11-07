c45fa70d879681063ee44b55debbe824dafee194
Jun Suzuki <84317349+szkjn@users.noreply.github.com>
2023-11-07 11:50:22 +0100

    Added initial question in comment.

--- swap_pairs.go
+++ swap_pairs.go
@@ -1,3 +1,11 @@
+/*
+Interview question of the week by Cassidy Williams (issue #260) :
+
+Given a list, swap every two adjacent nodes. Something to think 
+about: How would your code change if this were a linked list, 
+versus an array?
+*/
+
 package main
 
 import "fmt"
