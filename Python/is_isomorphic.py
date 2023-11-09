ee33e848159d873a7fe99ff505b82049f1a6cf13
Jun Suzuki <84317349+szkjn@users.noreply.github.com>
2023-11-09 15:20:16 +0100

    Create is_isomorphic.py

---
+++ Python/is_isomorphic.py
@@ -0,0 +1,30 @@
+"""
+Given two strings s and t, determine if they are isomorphic. Two strings are isomorphic if there is a one-to-one mapping possible for every character of the first string to every character of the second string.
+
+Example:
+
+> isIsomorphic('abb', 'cdd')
+> true // 'a' maps to 'c' and 'b' maps to 'd'
+
+> isIsomorphic('cassidy', '1234567')
+> false // 's' cannot have a mapping to both '3' and '4'
+
+> isIsomorphic('cass', '1233')
+> true
+"""
+
+def isIsomorphic(s: str, t: str) -> bool:
+    mapDict = {}
+    for i, _ in enumerate(s):
+        if s[i] not in mapDict.keys():
+            mapDict[s[i]] = t[i]
+        else:
+            if mapDict[s[i]] != t[i]:
+                return False
+    
+    return True
+
+
+print(isIsomorphic('abb', 'cdd'))
+print(isIsomorphic('cassidy', '1234567'))
+print(isIsomorphic('cass', '1233f'))
