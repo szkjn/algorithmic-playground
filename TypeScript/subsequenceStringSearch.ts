6c241f54afaa1c4067de11a6a22d5a1b8f8b05dc
junsera <junsuzuki@gmx.fr>
2023-11-12 21:52:37 +0100

    Added challenge description

--- TypeScript/subsequenceStringSearch.ts
+++ TypeScript/subsequenceStringSearch.ts
@@ -1,3 +1,27 @@
+/*
+We say that a string contains the word hackerrank if a subsequence
+of its characters spell the word hackerrank. Remember that a subsequence
+maintains the order of characters selected from a sequence.
+
+For each query, print YES on a new line if the string contains hackerrank,
+otherwise, print NO.
+
+Function Description
+- Complete the hackerrankInString function in the editor below.
+
+Parameter:
+- string s: a string
+
+Returns:
+- string: YES or NO
+
+Sample Input:
+
+hereiamstackerrank 	// should return YES
+hackerworld 		// should return NO
+hhaacckkekraraannk 	// should return YES
+*/
+
 function hackerrankInStringTS(s: string): string {
     const hackerrank: string = "hackerrank";
     let idx: number = 0;
