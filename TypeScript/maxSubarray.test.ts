eded0ee67167aeb89b29922204d90a45ee49ca76
junsera <junsuzuki@gmx.fr>
2023-11-22 16:59:55 +0100

    Added TypeScript testing to CI pipeline

---
+++ TypeScript/maxSubarray.test.ts
@@ -0,0 +1,24 @@
+import { sum, maxSubarray } from './maxSubarray';
+
+describe('sum', () => {
+  test('calculates the sum of array elements', () => {
+    expect(sum([1, 2, 3])).toBe(6);
+    expect(sum([-1, -2, -3])).toBe(-6);
+    expect(sum([])).toBe(0);
+  });
+});
+
+describe('maxSubarray', () => {
+  test('returns the subarray with the largest sum', () => {
+    expect(maxSubarray([-4, 2, -5, 1, 2, 3, 6, -5, 1], 4)).toEqual([2, 3, 6, -5]);
+    expect(maxSubarray([1, 2, 0, 5], 2)).toEqual([2, 0]);
+  });
+
+  test('handles cases where n is greater than array length', () => {
+    expect(maxSubarray([1, 2, 3], 5)).toEqual([1, 2, 3]);
+  });
+
+  test('handles empty array', () => {
+    expect(maxSubarray([], 3)).toEqual([]);
+  });
+});
