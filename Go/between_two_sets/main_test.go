34766862672aa814a1def61e03e7ae046c3dcb3a
junsera <junsuzuki@gmx.fr>
2023-11-27 23:06:40 +0100

    Refactored a few Go test files

--- Go/between_two_sets/main_test.go
+++ Go/between_two_sets/main_test.go
@@ -1,43 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"testing"
 )
 
+type TestCase struct {
+	Input    [][]int32 `json:"input"`
+	Expected int32     `json:"expected"`
+}
+
+func loadTestData(filePath string) ([]TestCase, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var testCases []TestCase
+	err = json.NewDecoder(file).Decode(&testCases)
+	if err != nil {
+		return nil, err
+	}
+
+	return testCases, nil
+}
+
 func TestGetTotalX(t *testing.T) {
-	testCases := []struct {
-		a        []int32
-		b        []int32
-		expected int32
-	}{
-		{
-			[]int32{2, 4},
-			[]int32{16, 32, 96},
-			3,
-		},
-		{
-			[]int32{3, 4},
-			[]int32{24, 48},
-			2,
-		},
-		{
-			[]int32{1},
-			[]int32{72, 48},
-			8,
-		},
-		{
-			[]int32{2, 3, 6},
-			[]int32{42, 84},
-			2,
-		},
+	testCases, err := loadTestData("../../test_data/between_two_sets.json")
+	if err != nil {
+		t.Fatalf("Failed to load test data: %v", err)
 	}
 
 	for _, tc := range testCases {
-		t.Run(fmt.Sprintf("a=%v,b=%v", tc.a, tc.b), func(t *testing.T) {
-			got := getTotalX(tc.a, tc.b)
-			if got != tc.expected {
-				t.Errorf("getTotalX(%v, %v) = %d; want %d", tc.a, tc.b, got, tc.expected)
+		t.Run(fmt.Sprintf("a=%v,b=%v", tc.Input[0], tc.Input[1]), func(t *testing.T) {
+			got := getTotalX(tc.Input[0], tc.Input[1])
+			if got != tc.Expected {
+				t.Errorf("getTotalX(%v, %v) = %d; want %d", tc.Input[0], tc.Input[1], got, tc.Expected)
 			}
 		})
 	}
