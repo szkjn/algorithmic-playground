ace4a81e877ea60ba3d3356b66cca7627142c8c9
junsera <junsuzuki@gmx.fr>
2023-12-01 22:39:10 +0100

    Added pair_counter.py

---
+++ Python/scripts/pair_counter.py
@@ -0,0 +1,46 @@
+"""
+Given an array of integers where each number represents a sock's color, the 
+function calculates the number of matching sock pairs. Each pair consists 
+of two socks of the same color. The function takes the total number of socks 
+and an array of their colors as input, and returns the total number of 
+matching pairs.
+
+Input Parameters:
+- int n: Number of socks in the pile.
+- int arr[n]: Array representing the colors of each sock.
+
+Output:
+- int: The total number of matching sock pairs.
+
+Constraints:
+- 1 ≤ n ≤ 100
+- 1 ≤ arr[i] ≤ 100 where 0 ≤ i < n
+"""
+
+import math
+import os
+import random
+import re
+import sys
+
+def pair_counter(n, arr):
+    countMap = {}
+    
+    for el in arr:
+        if el in countMap:
+            countMap[el] += 1
+        else:
+            countMap[el] = 1
+        
+    countPairs = 0
+    
+    for el in countMap.values():
+        countPairs += el // 2
+            
+    return int(countPairs)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    result = pair_counter(n, arr)
+    print(str(result))
---
+++ Python/tests/test_pair_counter.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import pytest
+
+PROJ_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+
+sys.path.append(PROJ_PATH)
+
+from Python.tests.test_utils import load_test_data
+from Python.scripts.pair_counter import pair_counter
+
+test_data = load_test_data("pair_counter.json")
+
+
+@pytest.mark.parametrize(
+    "n, arr, expected", [(item["n"], item["arr"], item["expected"]) for item in test_data]
+)
+def test_pair_counter(n, arr, expected):
+    assert pair_counter(n, arr) == expected
