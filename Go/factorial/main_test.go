a869217a64ddf41068b65b8a2bf2bcac79034a78
junsera <junsuzuki@gmx.fr>
2023-12-01 23:06:42 +0100

    Deleted JavaScript folder

--- Go/factorial/main_test.go
+++ Go/factorial/main_test.go
@@ -1,26 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"testing"
 )
 
+type TestCase struct {
+	N        int32 `json:"n"`
+	Expected int32 `json:"expected"`
+}
+
+func loadTestData(filePath string) ([]TestCase, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var testCases []TestCase
+	err = json.NewDecoder(file).Decode(&testCases)
+	if err != nil {
+		return nil, err
+	}
+
+	return testCases, nil
+}
 func TestFactorial(t *testing.T) {
-	testCases := []struct {
-		n        int32
-		expected int32
-	}{
-		{3, 6},
-		{1, 1},
-		{5, 120},
-		{11, 39916800},
+	testCases, err := loadTestData("../../test_data/factorial.json")
+	if err != nil {
+		t.Fatalf("Failed to load test data: %v", err)
 	}
 
 	for _, tc := range testCases {
-		t.Run(fmt.Sprintf("n=%d", tc.n), func(t *testing.T) {
-			got := factorial(tc.n)
-			if got != tc.expected {
-				t.Errorf("factorial(%d) = %d; want %d", tc.n, got, tc.expected)
+		t.Run(fmt.Sprintf("n=%d", tc.N), func(t *testing.T) {
+			got := factorial(tc.N)
+			if got != tc.Expected {
+				t.Errorf("factorial(%d) = %d; want %d", tc.N, got, tc.Expected)
 			}
 		})
 	}
