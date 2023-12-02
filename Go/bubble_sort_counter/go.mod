f962a52d4436d91944943dcc14e5e565ff534f89
junsera <junsuzuki@gmx.fr>
2023-12-02 18:43:57 +0100

    Added bubble_sort_counter (Go)

---
+++ Go/bubble_sort_counter/go.mod
@@ -0,0 +1,3 @@
+module github.com/szkjn/algorithmic-playground/Go/bubble_sort_counter
+
+go 1.21.4
---
+++ Go/bubble_sort_counter/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func findMinAndMax(a []int32) (min, max int32) {
+	min = a[0]
+	max = a[0]
+	for _, value := range a {
+		if value < min {
+			min = value
+		}
+		if value > max {
+			max = value
+		}
+	}
+	return min, max
+}
+
+func sort(arr []int32) ([]int32, int32) {
+	var count int32 = 0
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				count++
+			}
+		}
+	}
+	return arr, count
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var a []int32
+
+	for i := 0; i < int(n); i++ {
+		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+		checkError(err)
+		aItem := int32(aItemTemp)
+		a = append(a, aItem)
+	}
+
+	sortedArr, count := sort(a)
+	min, max := findMinAndMax(sortedArr)
+
+	fmt.Printf("Array is sorted in %d swaps.\n", count)
+	fmt.Printf("First Element: %d\n", min)
+	fmt.Printf("Last Element: %d\n", max)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
